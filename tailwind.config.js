5c9663a5b5feea507c7067b3bdafb2cf35fa266d
Richard <richardmusime6@gmail.com>
2024-05-17 12:23:43 +0200

    Adding News Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     extend: {
       letterSpacing: {
         'extra-tight': '-0.05em',
-      }
+      },
+      container: {
+        center: false,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          md: "4rem",
+          lg: "6rem",
+          xl: "8rem",
+          "2xl": "12rem",
+          "3xl": "16rem",
+          "4xl": "20rem",
+        },
+        margin: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          md: "4rem",
+          lg: "6rem",
+          xl: "8rem",
+          "2xl": "12rem",
+          "3xl": "16rem",
+          "4xl": "20rem",
+        },
+      },
     },
   },
   plugins: [],
